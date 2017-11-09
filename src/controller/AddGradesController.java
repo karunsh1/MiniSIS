ded924152b01fbfd7f0cdfe83b1c839ededf9797
karunsh1 <karunsh1@gmail.com>
2017-11-09 18:19:05 -0500

    Import and export CSV with query

--- src/controller/AddGradesController.java
+++ src/controller/AddGradesController.java
@@ -1,29 +1,29 @@
 package controller;
 
-import java.awt.Desktop;
+
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Window;
 import model.DAO;
 import util.Singleton;
 
 public class AddGradesController implements Initializable {
 	
-	private Desktop desktop = Desktop.getDesktop();
+	
 	@FXML
     private ComboBox cbSelectTerm;
 	@FXML
@@ -47,6 +47,8 @@ public class AddGradesController implements Initializable {
 	@FXML
     private Button btnUploadTemplate;
 	@FXML
+    private Label lblFileName;
+	@FXML
 	private Button btnFileChooser;
 	private Window stage;
 	
@@ -55,43 +57,8 @@ public class AddGradesController implements Initializable {
 	
 	@FXML
 	private void onSelectTerm(){
-		/*String sql =  "SELECT cd.id, ti.term as term,CONCAT(ins.first_name,' ',ins.last_name) as instructor,GROUP_CONCAT(CONCAT(sch.day,' ',sch.start_time,' - ',sch.end_time) SEPARATOR ' & ') as time,"
-				+ " CONCAT(bld.name,' ',rm.room_no,' ',bld.campus_short) as location, CONCAT(sbj.subject_code,' ',cs.course_code,' - ',cs.title) as course, "
-				+ " if((cd.class_capacity -(select count(*) from registration rgs where cd.id=rgs.course_details_id))>0,'Open','Closed') as status, cs.units as unit"
-				+ " FROM course_details cd, course cs, subject sbj, term_info ti, instructor ins, schedule sch, course_schedule csch, room rm, building bld, registration rc"
-				+ " WHERE rc.course_details_id=cd.id AND" + " cd.term_id=ti.id AND" + " ti.id=? AND"
-				+ " cd.instructor_id=ins.id AND" + " cd.room_id=rm.id AND" + " rm.building_id =bld.id AND"
-				+ " cd.course_id=cs.id AND" + " cs.subject_id=sbj.id and" + " csch.schedule_id=sch.id AND"
-				+ " csch.course_detail_id=cd.id" + " GROUP BY csch.course_detail_id";
-		MySQLAccess obj = new MySQLAccess();
-		Connection conn = obj.getConnection();
-		ArrayList enrollCourseList = null;
 
-		try {
-			PreparedStatement courseList = conn.prepareStatement(sql);
-			ResultSet result = courseList.executeQuery();
-			ArrayList Rows = new ArrayList();
-			while (result.next()) {
-				// System.out.println(dbID + dbName + dbStatusClient);
-				ArrayList row = new ArrayList();
-				for (int i = 1; i <= 1; i++) {
-					row.add(result.getString("id"));
-					row.add(result.getString("course"));
-					row.add(result.getString("instructor"));
-					row.add(result.getString("term"));
-					row.add(result.getString("time"));
-					row.add(result.getString("location"));
-					row.add(result.getString("unit"));
-					row.add(result.getString("status"));
-				}
-				Rows.add(row);
-				enrollCourseList = Rows;
-			}
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+
 		
 	}
 	@FXML
@@ -107,7 +74,15 @@ public class AddGradesController implements Initializable {
 	private void onSelectCareer(){		
 	}
 	@FXML
-	private void downnloadCSV(){		
+	private void downnloadCSV(){	
+		
+		String termID = "1";
+		String instructorID = Singleton.getInstance().getUserAcessID().getText(); 
+		String userHomeFolder = System.getProperty("user.home");
+		DAO dataAccess = new DAO();
+		dataAccess.exportCSVStudent_Inst_uploadGrades(userHomeFolder, instructorID, termID);
+		
+		
 	}
 	// For upload CSV 
 	@FXML
@@ -126,32 +101,40 @@ public class AddGradesController implements Initializable {
 	private void onSelectCareerUpload(){		
 	}
 	@FXML
-	private String clickOnFileChooser(){
-		String fileName = "GradeTemplate.csv";
+	private void clickOnFileChooser(){
+		String fileName = "Student_Result.csv";
+		String selectedPath = null;
+		
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Select Grade Template");
 		File file = fileChooser.showOpenDialog(stage);
-		fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));				
-		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("","*.CSV"));
+		fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));		
+		//fileChooser.getExtensionFilters().addAll(new ExtensionFilter("file","*.csv"));
 		
 		
-		 if (file != null) {
-             openFile(file);
-         }
-		String selectedPath  = file.getPath();
+		 if (file != null ) {
+			 selectedPath  = file.getPath();
+			 lblFileName.setText(selectedPath);
+			// lblFileName.textProperty().setValue(selectedPath);
+			 System.out.println("file path" + selectedPath +"   File Name   " + file.getName() +"label value " + lblFileName);
+         }		
 		
-		if(fileChooser.getSelectedExtensionFilter().getExtensions().equals(fileName)){
-			
-			System.out.println(file);
-			
-		}else{
-			System.out.println("wrong file");
-		}
+	
 				
-		return selectedPath;
+		
 	}
 	@FXML
-	private void UploadCSV(){		
+	private void UploadCSV(){	
+		String  filepath = null;
+		filepath = lblFileName.getText();
+		DAO dataAcess = new DAO();
+		try {
+			dataAcess.importCSVGPA_Instrutor(filepath);
+		} catch (FileNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -185,16 +168,8 @@ public class AddGradesController implements Initializable {
 		
 		
 	}
-	private void openFile(File file) {
-        try {
-            desktop.open(file);
-        } catch (IOException ex) {
-            Logger.getLogger(
-            		clickOnFileChooser()).log(
-                    Level.SEVERE, null, ex
-                );
-        }
-    }
+	
+    
 	
 	
 	
--- src/model/DAO.java
+++ src/model/DAO.java
@@ -2,12 +2,16 @@ package model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
 import DTO.Admin;
 import DTO.Instructor;
@@ -18,7 +22,6 @@ public class DAO {
 
 	public Term termList(int studentId) {
 		Term terminfo = null;
-		
 
 		try {
 
@@ -83,7 +86,6 @@ public class DAO {
 	public ArrayList termNames() {
 		String sql = null;
 		ArrayList termNameList = new ArrayList();
-		
 
 		sql = "select term from term_info";
 		MySQLAccess obj = new MySQLAccess();
@@ -91,12 +93,12 @@ public class DAO {
 		try {
 			PreparedStatement term = conn.prepareStatement(sql);
 			ResultSet result = term.executeQuery();
-			
+
 			while (result.next()) {
 				for (int i = 1; i <= 1; i++) {
-					termNameList.add(result.getString("term"));					
+					termNameList.add(result.getString("term"));
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -106,16 +108,16 @@ public class DAO {
 		return termNameList;
 
 	}
-	
+
 	/**
 	 * Department Name
+	 * 
 	 * @return
 	 */
-	
+
 	public ArrayList departmentNames() {
 		String sql = null;
 		ArrayList departmentList = new ArrayList();
-		
 
 		sql = "select subject_code from subject";
 		MySQLAccess obj = new MySQLAccess();
@@ -123,12 +125,12 @@ public class DAO {
 		try {
 			PreparedStatement term = conn.prepareStatement(sql);
 			ResultSet result = term.executeQuery();
-			
+
 			while (result.next()) {
 				for (int i = 1; i <= 1; i++) {
-					departmentList.add(result.getString("subject_code"));					
+					departmentList.add(result.getString("subject_code"));
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -138,51 +140,52 @@ public class DAO {
 		return departmentList;
 
 	}
-	
+
 	/**
 	 * Instructor ID
 	 * 
 	 */
-	
+
 	public Instructor selectInstructor(String email) {
 		Instructor instructor = null;
 		String sql = "select * from instructor where email=?";
-		
+
 		/// Establish Connection
 		MySQLAccess obj = new MySQLAccess();
 		Connection connection = obj.getConnection();
-		
+
 		try {
 			PreparedStatement prepareStm = connection.prepareStatement(sql);
 			prepareStm.setString(1, email);
 			ResultSet results = prepareStm.executeQuery();
-			while(results.next()){
+			while (results.next()) {
 				email = results.getString("email");
-				
+
 				int id = results.getInt("id");
 				String first_name = results.getString("first_name");
 				String last_name = results.getString("last_name");
-				//String mobile = results.getString("mobile");
+				// String mobile = results.getString("mobile");
 				String address = results.getString("address");
 				int emp_id = results.getInt("emp_id");
-	
+
 				instructor = new Instructor(id, first_name, last_name, email, emp_id, address);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return instructor;
 	}
+
 	/**
 	 * Course Name for Instructor
 	 */
 	public ArrayList Instructor_Courses(int instrutorID) {
 		String sql = null;
 		ArrayList CoursetList = new ArrayList();
-		
 
-		sql = "select course_code,title from course where id in(select course_id from course_details where instructor_id="+ instrutorID +")";
+		sql = "select course_code,title from course where id in(select course_id from course_details where instructor_id="
+				+ instrutorID + ")";
 		MySQLAccess obj = new MySQLAccess();
 		Connection conn = obj.getConnection();
 		try {
@@ -194,9 +197,9 @@ public class DAO {
 				for (int i = 1; i <= 1; i++) {
 					CourseID = result.getInt("course_code");
 					CourseTitle = result.getString("title");
-					CoursetList.add(CourseID+"-"+ CourseTitle);					
+					CoursetList.add(CourseID + "-" + CourseTitle);
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -206,6 +209,7 @@ public class DAO {
 		return CoursetList;
 
 	}
+
 	/**
 	 * instructor Department where he teaches
 	 * 
@@ -214,21 +218,22 @@ public class DAO {
 	public ArrayList Instructor_Dept(int instrutorID) {
 		String sql = null;
 		ArrayList deptList = new ArrayList();
-		
 
-		sql = "select subject_code from subject where id in (select subject_id from course where  id  in( select course_id  from course_details where instructor_id="+ instrutorID +"))";
+		sql = "select subject_code from subject where id in (select subject_id from course where  id  in( select course_id  from course_details where instructor_id="
+				+ instrutorID + "))";
 		MySQLAccess obj = new MySQLAccess();
 		Connection conn = obj.getConnection();
 		try {
 			PreparedStatement term = conn.prepareStatement(sql);
 			ResultSet result = term.executeQuery();
-			
+
 			while (result.next()) {
 				for (int i = 1; i <= 1; i++) {
-					
-					deptList.add(result.getString("subject_code"));;					
+
+					deptList.add(result.getString("subject_code"));
+					;
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -238,28 +243,30 @@ public class DAO {
 		return deptList;
 
 	}
+
 	/**
 	 * Career Leve Grad or UnderGrad
 	 */
-	
+
 	public ArrayList careerList_ins(int instrutorID) {
 		String sql = null;
 		ArrayList deptList = new ArrayList();
-		
 
-		sql = "select level from minisis.course where id in (select course_id from minisis.course_details where instructor_id ="+ instrutorID +")";
+		sql = "select level from minisis.course where id in (select course_id from minisis.course_details where instructor_id ="
+				+ instrutorID + ")";
 		MySQLAccess obj = new MySQLAccess();
 		Connection conn = obj.getConnection();
 		try {
 			PreparedStatement term = conn.prepareStatement(sql);
 			ResultSet result = term.executeQuery();
-			
+
 			while (result.next()) {
 				for (int i = 1; i <= 1; i++) {
-					
-					deptList.add(result.getString("level"));;					
+
+					deptList.add(result.getString("level"));
+					;
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -269,41 +276,95 @@ public class DAO {
 		return deptList;
 
 	}
-	
+
 	/**
 	 * CSV Export from Database
 	 */
-	public File exportCSVStudent_Inst_uploadGrades(String filePath, String IntructorID ,String termID ){
-		
-		String fileName = "Student_Result";
+	public void exportCSVStudent_Inst_uploadGrades(String filePath, String IntructorID, String termID) {
+
+		String fileName = filePath + "\\" + "Student_Result.csv";
 		String sql = "";
-		
-				
+
 		MySQLAccess obj = new MySQLAccess();
 		Connection conn = obj.getConnection();
-		sql = "Select id,first_name,last_name from minisis.student where id in(select student_id from minisis.registration where status ='enrolled' and"
-				+ "  course_details_id in(select id from minisis.course_details where instructor_id ="+IntructorID+" and term_id ="+termID+"))";
-		
-		try {
-			PrintWriter pw = new PrintWriter(new File(filePath+ "\\" +fileName));
-			StringBuilder sb = new StringBuilder();
-	        sb.append("id");
-	        sb.append(',');
-	        sb.append("first_name");
-	        sb.append(',');
-	        
+		sql = "Select id,first_name,last_name from student where id in(select student_id from registration where status ='enrolled' and"
+				+ "  course_details_id in(select id from course_details where instructor_id =" + IntructorID
+				+ " and term_id =" + termID + "))";
+		boolean alreadyExists = new File(fileName).exists();
 
-			
-		} catch (FileNotFoundException e) {
+		try {
+			CsvWriter csvStudentFile = new CsvWriter(new FileWriter(fileName, true), ',');
+			if (!alreadyExists) {
+				csvStudentFile.write("Student_ID");
+				csvStudentFile.write("First_Name");
+				csvStudentFile.write("Last_name");
+				csvStudentFile.write("GPA");
+				csvStudentFile.endRecord();
+			}
+
+			PreparedStatement studentInfo = conn.prepareStatement(sql);
+			ResultSet result = studentInfo.executeQuery();
+			while (result.next()) {
+				for (int i = 1; i <= 1; i++) {
+					csvStudentFile.write(result.getString("id"));
+					csvStudentFile.write(result.getString("first_Name"));
+					csvStudentFile.write(result.getString("last_name"));
+					csvStudentFile.endRecord();
+				}
+
+			}
+			csvStudentFile.close();
+			System.out.println("Done");
+
+		} catch (IOException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+	}
+	
+	public void importCSVGPA_Instrutor(String filePath) throws FileNotFoundException, SQLException{
 		
-		return null;
+	CsvReader csvGPAFile;
+	String strudentID = null;
+	String studentGPA = null;
+	String selectsql = "";
+	String insertsql = "";
+	
+	MySQLAccess obj = new MySQLAccess();
+	Connection conn = obj.getConnection();
+	
+	
+	
+	
+	try {
+		csvGPAFile = new CsvReader(filePath);
+		csvGPAFile.readHeaders();
+		while(csvGPAFile.readRecord()){			
+			strudentID = csvGPAFile.get("Student_ID");
+			selectsql = "SELECT student_id,gpa FROM grade where student_id "+strudentID+" course_id in( select course_id from course_details where id in(select course_details_id from registration))";
+			PreparedStatement selectStatement = conn.prepareStatement(selectsql);
+			if(selectStatement.executeQuery().absolute(1))
+			{
+				studentGPA = csvGPAFile.get("GPA");
+				selectStatement.setString(2, studentGPA);
+			}
+			
+			
+			
+			
+			
+		}
 		
+		
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	
+	
+				
 		
 	}
-		
-	
 
 }
