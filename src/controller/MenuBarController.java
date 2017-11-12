958bc5bbfce4efe6f8e8373b4155506d46e08b88
nidsarora <aroranidhi10@gmail.com>
2017-11-12 03:41:35 -0500

    Populating comboboxes in search course

--- src/controller/MenuBarController.java
+++ src/controller/MenuBarController.java
@@ -26,10 +26,7 @@ public class MenuBarController implements Initializable {
 	@FXML
 	private MenuBar menuBar;
 	@FXML
-	private MenuItem mItemViewTranscript;
-	
-
-   
+	private MenuItem mItemViewTranscript;  
     @FXML
 	private MenuItem menuitemSearchCourse;
    
@@ -38,7 +35,7 @@ public class MenuBarController implements Initializable {
 		BorderPane newLoadedPane = null;
 		try {
 			
-			newLoadedPane = FXMLLoader.load(getClass().getResource("/view/MenuBar.fxml"));
+			newLoadedPane = FXMLLoader.load(getClass().getResource("/view/SearchCourse1.fxml"));
 		} catch (IOException e) {
 			System.out.println("in catch");
 			// TODO Auto-generated catch block
